46f7da75b1082668e6d76691639e049b42573a33
José Jaime Ariza <jjaime@ikergune.com>
2015-06-12 16:00:53 +0200

    Fixed a bug in Json message sanitization. Changed some error messages.

--- src/rest.cpp
+++ src/rest.cpp
@@ -1,8 +1,15 @@
+/*!
+ *  \brief     Sends rosout_agg to Flume via Rest
+ *  \author    José Jaime Ariza (jjaime@ikergune.com)
+ *  \copyright See LICENSE file
+ */
+
 #include "ros/ros.h"
 #include "rosgraph_msgs/Log.h"
 #include "std_msgs/Header.h"
 #include <sstream>
 #include <iostream>
+#include <vector>
 #include <unistd.h>
 #include <curl/curl.h>
 
@@ -22,10 +29,16 @@ size_t curlcb(void *ptr, size_t size, size_t nmemb, void *data) {
  */
 void logCallback(const rosgraph_msgs::Log::ConstPtr& msg) {
     std::stringstream jsonmsg;
-    std::string jsonstr;
+    std::string jsonstr, msgstr;
     CURL *curlh;
     CURLcode curlstatus;
     long http_code;
+
+    //Process msg string
+    msgstr = msg->msg;
+    std::replace(msgstr.begin(), msgstr.end(), '"', '\'');
+    std::replace(msgstr.begin(), msgstr.end(), '\n', ' ');
+    msgstr.erase(std::remove(msgstr.begin(), msgstr.end(), '\r'), msgstr.end());
 
     //Create JSON message
     jsonmsg << "[{";
@@ -38,13 +51,13 @@ void logCallback(const rosgraph_msgs::Log::ConstPtr& msg) {
     jsonmsg << "\"function\":\"" << msg->function << "\",";
     jsonmsg << "\"line\":" << msg->line;
     jsonmsg << "},";
-    jsonmsg << "\"body\":\"" << msg->msg << "\"";
+    jsonmsg << "\"body\":\"" << msgstr << "\"";
     jsonmsg << "}]";
 
     //Send it to flume via REST
     curlh = curl_easy_init();
     if (!curlh) {
-        fprintf(stderr, "curl_easy_init(): error\n");
+        fprintf(stderr, "curl_easy_init(): error, resturl: %s\n", resturl.c_str());
     }
     else {
         curl_easy_setopt(curlh, CURLOPT_URL, resturl.c_str());
@@ -54,16 +67,17 @@ void logCallback(const rosgraph_msgs::Log::ConstPtr& msg) {
         curl_easy_setopt(curlh, CURLOPT_POSTFIELDSIZE_LARGE, (curl_off_t)jsonstr.length());
         curlstatus = curl_easy_perform(curlh);
         if(curlstatus != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(curlstatus));
+            fprintf(stderr, "curl_easy_perform() failed: %s, resturl: %s\n", curl_easy_strerror(curlstatus), resturl.c_str());
         }
         else {
             curl_easy_getinfo(curlh, CURLINFO_RESPONSE_CODE, &http_code);
             if (http_code != 200) {
-                fprintf(stderr, "Return status not 200\n");    
+                fprintf(stderr, "Return status not 200 (%li), resturl: %s\n", http_code, resturl.c_str());
             }
         }
     }
     curl_easy_cleanup(curlh);
+
 }
 
 int main(int argc, char **argv) {
